34f34e00bd849b94ec3048ab8132162c4d654fd3
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-05-08 17:51:44 +0800

    fix: standardize quotes and formatting across multiple files
    - Updated layout.tsx to include a missing line break.
    - Corrected LINE_CONFIG object syntax in line.ts.
    - Standardized quotes in products.ts for consistency.
    - Improved error handling and formatting in line-expire-products-broadcast.js and line-webhook.ts.
    - Refactored line-bot-services.js for better readability and consistency in object properties.

--- src/constants/products.ts
+++ src/constants/products.ts
@@ -2,20 +2,20 @@
 export const MOCK_SPECIAL_PRODUCTS_DATA = [
   {
     id: 1,
-    name: '魔方戀柱',
+    name: "魔方戀柱",
     price: 10000,
-    img: 'https://docs.1chooo.com/images/cover-transparent-with-1chooo-com.png',
+    img: "https://docs.1chooo.com/images/cover-transparent-with-1chooo-com.png",
   },
   {
     id: 2,
-    name: '魔方善意',
+    name: "魔方善意",
     price: 13000,
-    img: 'https://docs.1chooo.com/images/cover-transparent-with-1chooo-com.png',
+    img: "https://docs.1chooo.com/images/cover-transparent-with-1chooo-com.png",
   },
   {
     id: 3,
-    name: 'Jacksdo 魯夫vs凱多',
+    name: "Jacksdo 魯夫vs凱多",
     price: 15000,
-    img: 'https://docs.1chooo.com/images/cover-transparent-with-1chooo-com.png',
+    img: "https://docs.1chooo.com/images/cover-transparent-with-1chooo-com.png",
   },
-]
+];
--- src/pages/api/line-expire-products-broadcast.js
+++ src/pages/api/line-expire-products-broadcast.js
@@ -1,17 +1,15 @@
-import {
-  getExpireProductsListBroadcast
-} from '@/services/line-bot-services'
+import { getExpireProductsListBroadcast } from "@/services/line-bot-services";
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     /* 透過  broadcast 對群裡所有人進行商品到期通知 */
     getExpireProductsListBroadcast()
       .then(() => res.status(200).end())
       .catch((err) => {
-        console.log(err)
-        res.status(500).end()
-      })
+        console.log(err);
+        res.status(500).end();
+      });
   } else {
-    res.status(405).end()
+    res.status(405).end();
   }
 }
