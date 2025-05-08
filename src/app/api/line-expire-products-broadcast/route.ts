1cf0122682497bb67e7a1d0c4a4ec76bfccc283b
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-05-09 00:03:38 +0800

    feat: improve response handling in line-expire-products-broadcast route

--- src/app/api/line-expire-products-broadcast/route.ts
+++ src/app/api/line-expire-products-broadcast/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+
 import { getExpireProductsListBroadcast } from "@/services/line-bot-services";
 
 // Ensure the route is always dynamically rendered and not cached
@@ -8,26 +9,32 @@ export async function POST() {
   try {
     /* Broadcast expired product notifications to all users in the group */
     await getExpireProductsListBroadcast();
-    return new NextResponse(null, { status: 200 });
+    return NextResponse.json(
+      { message: "Broadcast successful" },
+      { status: 200 }
+    );
   } catch (err) {
     console.log(err);
-    return new NextResponse(null, { status: 500 });
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
 
 // Handle other HTTP methods
 export async function GET() {
-  return new NextResponse(null, { status: 405 });
+  return NextResponse.json({ message: "Method Not Allowed" }, { status: 405 });
 }
 
 export async function PUT() {
-  return new NextResponse(null, { status: 405 });
+  return NextResponse.json({ message: "Method Not Allowed" }, { status: 405 });
 }
 
 export async function DELETE() {
-  return new NextResponse(null, { status: 405 });
+  return NextResponse.json({ message: "Method Not Allowed" }, { status: 405 });
 }
 
 export async function PATCH() {
-  return new NextResponse(null, { status: 405 });
+  return NextResponse.json({ message: "Method Not Allowed" }, { status: 405 });
 }
