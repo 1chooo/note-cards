b42d8dc399d0366739d580a0b4caf7e8a89a133b
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-05-09 15:18:47 +0800

    style: clean up code formatting and improve consistency across files

--- src/lib/line.ts
+++ src/lib/line.ts
@@ -7,19 +7,18 @@ import { messagingApi } from "@line/bot-sdk";
 
 const { MessagingApiClient } = messagingApi;
 
-// Setup all LINE client and Express configurations.
 export const clientConfig: ClientConfig = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || "",
   channelSecret: process.env.CHANNEL_SECRET,
 };
+
+export const client = new MessagingApiClient({
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || "",
+});
 
 const middlewareConfig: MiddlewareConfig = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET || "",
 };
 
-// export const client = new Client(clientConfig);
-export const client = new MessagingApiClient({
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || "",
-});
 export const middleware = lineMiddleware(middlewareConfig);
