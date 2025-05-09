5a74032442366245684156bebc3af9b5183c4fb4
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-05-09 15:40:47 +0800

    refactor(line): replace deprecated Client with MessagingApiClient and update message handling (#1)
    - resolved: #1

--- src/services/line-bot-services.ts
+++ src/services/line-bot-services.ts
@@ -1,19 +1,19 @@
-import { Client, middleware } from "@line/bot-sdk";
-import { WebhookEvent } from "@line/bot-sdk";
+import { middleware } from "@line/bot-sdk";
+import { WebhookEvent, messagingApi } from "@line/bot-sdk";
 
 import { LINE_CONFIG } from "@/constants/line";
 import { MOCK_SPECIAL_PRODUCTS_DATA } from "@/constants/products";
 
-// client 主要是用來對 LINE Bot 進行操作。ex. 回覆、廣播
+const { MessagingApiClient } = messagingApi;
+
+const client = new MessagingApiClient({
+  channelAccessToken: LINE_CONFIG.channelAccessToken || "",
+});
+
 if (!LINE_CONFIG.channelAccessToken || !LINE_CONFIG.channelSecret) {
   throw new Error("LINE_CONFIG is missing required properties.");
 }
-export const client = new Client({
-  channelAccessToken: LINE_CONFIG.channelAccessToken,
-  channelSecret: LINE_CONFIG.channelSecret,
-});
 
-// middleware 是來驗證 webhook 回傳時的 x-line-signature 是否合法。
 export const lineMiddleware = middleware({
   channelAccessToken: LINE_CONFIG.channelAccessToken!,
   channelSecret: LINE_CONFIG.channelSecret!,
@@ -26,26 +26,34 @@ export const getRandomSpecialProductsMessage = async (event: WebhookEvent) => {
 
   const randomProduct =
     MOCK_SPECIAL_PRODUCTS_DATA[
-      Math.floor(Math.random() * MOCK_SPECIAL_PRODUCTS_DATA.length)
+    Math.floor(Math.random() * MOCK_SPECIAL_PRODUCTS_DATA.length)
     ];
-  return client.replyMessage(event.replyToken, [
-    {
-      type: "text",
-      text: `特價商品 $ 清單如下：\n\n ${randomProduct.name} 金額＄ ${randomProduct.price}`,
-      emojis: [
+
+  const userId = event.source.userId;
+  return (
+    await client.pushMessage({
+      to: userId ?? "",
+      messages: [
         {
-          index: 5, // 看 $ 這個符號位於文字中的第幾個位子 (index 從 0 開始)
-          productId: "5ac21a8c040ab15980c9b43f",
-          emojiId: "067",
+          type: "text",
+          text: `特價商品 $ 清單如下：\n\n ${randomProduct.name} 金額＄ ${randomProduct.price}`,
+          emojis: [
+            {
+              index: 5, // 看 $ 這個符號位於文字中的第幾個位子 (index 從 0 開始)
+              productId: "5ac21a8c040ab15980c9b43f",
+              emojiId: "067",
+            },
+          ],
+        },
+        {
+          type: "image",
+          originalContentUrl: randomProduct.img,
+          previewImageUrl: randomProduct.img,
         },
       ],
-    },
-    {
-      type: "image",
-      originalContentUrl: randomProduct.img,
-      previewImageUrl: randomProduct.img,
-    },
-  ]);
+    }
+    ));
+
 };
 
 export const getExpireProductsListBroadcast = async () => {
@@ -54,13 +62,17 @@ export const getExpireProductsListBroadcast = async () => {
       `商品編號: ${product.id} , ${product.name} 金額＄ ${product.price}`
   ).join(`\n`);
   return client.broadcast({
-    type: "text",
-    text: `本月 即將到期商品 $ 清單如下：\n\n${productString} \n\n請操作人員協助後續處理，謝謝！`,
-    emojis: [
+    messages: [
       {
-        index: 10, // index 代表 $ 符號所在的的位置，以上面為例：『本月 即將到期商品 $』 前字號位於第 10 個字元
-        productId: "5ac2213e040ab15980c9b447", // Doc: https://developers.line.biz/en/docs/messaging-api/emoji-list/
-        emojiId: "005",
+        type: "text",
+        text: `本月 即將到期商品 $ 清單如下：\n\n${productString} \n\n請操作人員協助後續處理，謝謝！`,
+        emojis: [
+          {
+            index: 10, // index 代表 $ 符號所在的的位置，以上面為例：『本月 即將到期商品 $』 前字號位於第 10 個字元
+            productId: "5ac2213e040ab15980c9b447", // Doc: https://developers.line.biz/en/docs/messaging-api/emoji-list/
+            emojiId: "005",
+          },
+        ],
       },
     ],
   });
